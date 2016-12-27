b930ceee157486a739f137c5fe70c1699a6decf8
bxcar <malanchukdima@mail.ru>
2016-12-27 16:05:11 +0200

    some changes

--- wp-content/themes/sydney/footer.php
+++ wp-content/themes/sydney/footer.php
@@ -7,102 +7,133 @@
  * @package Sydney
  */
 ?>
-<p class="prices-info">Ціна вказана на одного при двумісному розміщенні. Всі збори вже включені у вартість! По деяким напрямкам необхідно
+<p class="prices-info">Ціна вказана на одного при двумісному розміщенні. Всі збори вже включені у вартість! По деяким
+    напрямкам необхідно
     відкриття візи. Ціни реальні, але місця закінчуються</p>
-<p class="get-offers-button"><a onclick="get_all_button();" class="popmake-form_for_special btn1 modal-link product-link"
+<p class="get-offers-button"><a onclick="get_all_button();"
+                                class="popmake-form_for_special btn1 modal-link product-link"
                                 data-order="Получить все предложения">Отримати всі пропозиції</a></p>
 </div>
 </div>
 <section class="prices">
-    <p class="get-offers-button" style="margin-top: 20px; margin-bottom: 10px;"><a style="background-color: #fff; color: #3e3e3e;"onclick="get_all_button();" class="popmake-form_for_special btn1 modal-link product-link"
-                                    data-order="Получить все предложения">Залиште запит</a></p>
+    <p class="get-offers-button" style="margin-top: 20px; margin-bottom: 10px;"><a
+            style="background-color: #fff; color: #3e3e3e;" onclick="get_all_button();"
+            class="popmake-form_for_special btn1 modal-link product-link"
+            data-order="Получить все предложения">Залиште запит</a></p>
     <span style="color: #252525; font-size: 16px; line-height: 18px;" class="prefooter">та отримайте сертифікат на суму 100грн., який діє на слідуючих туроператорів</span>
     <img id="palmright" src="/wp-content/themes/sydney/img/palm.png">
     <img id="palmleft" src="/wp-content/themes/sydney/img/palmleft.png">
+    <ul class="req-list">
+        <li><img src="/wp-content/themes/sydney/img/tui-logo.png" alt="tui"/></li>
+        <li><img src="/wp-content/themes/sydney/img/logotez.png" alt="tez-tour"/></li>
+        <li><img style="width: 80px !important;" src="/wp-content/themes/sydney/img/anex-logo.png" alt="anex tour"/></li>
+        <li><img src="/wp-content/themes/sydney/img/joinup_logo.png" alt="join up"/></li>
+        <li><img style="width: 150px !important;" src="/wp-content/themes/sydney/img/pegas_logo.png" alt="pegas touristic"/></li>
+        <li><img style="width: 150px !important;" src="/wp-content/themes/sydney/img/coral_logo.png" alt="coral travel"/></li>
+    </ul>
     <div class="grid">
         <span class="prefooter">Залишилось всього 7 сертифікатів</span>
     </div>
 </section>
-<section class="team">
-    <div class="grid">
+<div id="content" class="page-wrap" style="padding-top: 30px; padding-bottom: 0;">
+    <div class="container content-wrapper">
+        <section class="team">
+            <div class="grid">
 
-        <h2>Перевірте нас прямо зараз!</h2>
-        <p class="team-text">Наші експерти підберуть і розрахують для вас 5 варіантів відпочинку за 10&nbsp;хвилин.</p>
+                <h2>Перевірте нас прямо зараз!</h2>
+                <p class="team-text">Наші експерти підберуть і розрахують для вас 5 варіантів відпочинку за 10&nbsp;хвилин.</p>
 
-        <div class="team-item box">
-            <div class="team-img Dunai"></div>
-            <div class="team-info">
-                <h3>Дунай Наталія</h3>
+                <div class="team-item box">
+                    <div class="team-img Dunai"></div>
+                    <div class="team-info">
+                        <h3>Дунай Наталія</h3>
 
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">+38 068 528-22-27<br><span style="font-size: 12px"></span></p>
-                <p class="team-mail">nd@makintour.com</p>
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png')">8 років в туризмі<br></p>
-                <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
+                        <p class="team-tel"
+                           style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">
+                            +38 068 528-22-27<br><span style="font-size: 12px"></span></p>
+                        <p class="team-mail">nd@makintour.com</p>
+                        <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png')">8
+                            років в туризмі<br></p>
+                        <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
 
-<!--                <p class="team-year">1 год в туризме</p>-->
+                        <!--                <p class="team-year">1 год в туризме</p>-->
 
-                <a href="#" class="popmake-129 modal-link product-link img-link"
-                   data-order="Проверьте нас (Наталия)"
-                   data-img="http://www.makintour.com/uploads/Dunai.jpg">Задайте мені питання</a>
+                        <a href="#" class="popmake-129 modal-link product-link img-link"
+                           data-order="Проверьте нас (Наталия)"
+                           data-img="http://www.makintour.com/uploads/Dunai.jpg">Задайте мені питання</a>
+                    </div>
+                </div>
+
+                <div class="team-item box">
+                    <div class="team-img Desyatnik">
+                    </div>
+                    <div class="team-info">
+                        <h3>Десятник Володимир</h3>
+
+                        <p class="team-tel"
+                           style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">
+                            +38 068 528-22-27<br><span style="font-size: 12px"></span></p>
+                        <p class="team-mail">tour0303@makintour.com</p>
+                        <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png');">7
+                            років в туризмі<br></p>
+                        <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
+
+                        <!--                <p class="team-year">2 года в туризме</p>-->
+
+                        <a href="#" class="popmake-599 modal-link product-link img-link"
+                           data-order="Проверьте нас (Владимир)"
+                           data-img="http://www.makintour.com/uploads/Desyatnik.JPG">Задайте мені питання</a>
+                    </div>
+                </div>
+
+                <div class="team-item box">
+                    <div class="team-img Chorna"></div>
+                    <div class="team-info">
+                        <h3>Чорна Юлія</h3>
+
+                        <p class="team-tel"
+                           style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">
+                            +38 068 528-22-27<br><span style="font-size: 12px"><!--(Viber, WhatsApp)--></span></p>
+                        <p class="team-mail">tour0301@makintour.com</p>
+                        <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png');">6
+                            років в туризмі<br></p>
+                        <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
+
+                        <!--                <p class="team-year">1 год в туризме</p>-->
+
+                        <a href="#" class="popmake-597 modal-link product-link img-link"
+                           data-order="Проверьте нас (Юлия)"
+                           data-img="http://www.makintour.com/uploads/Chorna.jpg">Задайте мені питання</a>
+                    </div>
+                </div>
+
+                <div class="team-item box">
+                    <div class="team-img Chorna"></div>
+                    <div class="team-info">
+                        <h3>Корольчук Наталія</h3>
+
+                        <p class="team-tel"
+                           style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">
+                            +38 068 528-22-27<br><span style="font-size: 12px"><!--(Viber, WhatsApp)--></span></p>
+                        <p class="team-mail">bulgaria@makintour.com</p>
+                        <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png');">8
+                            років в туризмі<br></p>
+                        <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
+
+                        <!--                <p class="team-year">1 год в туризме</p>-->
+
+                        <a href="#" class="popmake-597 modal-link product-link img-link"
+                           data-order="Проверьте нас (Юлия)"
+                           data-img="http://www.makintour.com/uploads/Chorna.jpg">Задайте мені питання</a>
+                    </div>
+                </div>
             </div>
-        </div>
-
-        <div class="team-item box">
-            <div class="team-img Desyatnik">
-            </div>
-            <div class="team-info">
-                <h3>Десятник Володимир</h3>
-
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">+38 068 528-22-27<br><span style="font-size: 12px"></span></p>
-                <p class="team-mail">tour0303@makintour.com</p>
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png');">7 років в туризмі<br></p>
-                <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
-
-<!--                <p class="team-year">2 года в туризме</p>-->
-
-                <a href="#" class="popmake-599 modal-link product-link img-link"
-                   data-order="Проверьте нас (Владимир)"
-                   data-img="http://www.makintour.com/uploads/Desyatnik.JPG">Задайте мені питання</a>
-            </div>
-        </div>
-
-        <div class="team-item box">
-            <div class="team-img Chorna"></div>
-            <div class="team-info">
-                <h3>Чорна Юлія</h3>
-
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">+38 068 528-22-27<br><span style="font-size: 12px"><!--(Viber, WhatsApp)--></span></p>
-                <p class="team-mail">tour0301@makintour.com</p>
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png');">6 років в туризмі<br></p>
-                <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
-
-<!--                <p class="team-year">1 год в туризме</p>-->
-
-                <a href="#" class="popmake-597 modal-link product-link img-link"
-                   data-order="Проверьте нас (Юлия)"
-                   data-img="http://www.makintour.com/uploads/Chorna.jpg">Задайте мені питання</a>
-            </div>
-        </div>
-
-        <div class="team-item box">
-            <div class="team-img Chorna"></div>
-            <div class="team-info">
-                <h3>Корольчук Наталія</h3>
-
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/telephone-icon-for-manager.png') !important;">+38 068 528-22-27<br><span style="font-size: 12px"><!--(Viber, WhatsApp)--></span></p>
-                <p class="team-mail">bulgaria@makintour.com</p>
-                <p class="team-tel" style="background-image: url('/wp-content/themes/sydney/img/icon4.png');">8 років в туризмі<br></p>
-                <!--<p class="viber-whatsapp"><span>(Viber, WhatsApp)</span> +38 099 213-95-55</p>-->
-
-                <!--                <p class="team-year">1 год в туризме</p>-->
-
-                <a href="#" class="popmake-597 modal-link product-link img-link"
-                   data-order="Проверьте нас (Юлия)"
-                   data-img="http://www.makintour.com/uploads/Chorna.jpg">Задайте мені питання</a>
-            </div>
-        </div>
+            <p class="telephone2">
+                <span>Є запитання про відпочинок? З радістю допоможем:</span>
+                +38(068) 528 2227</p>
+        </section>
     </div>
-</section>
+</div>
 
 </div>
 </div>
@@ -173,31 +204,31 @@
                 </div>-->
 <!--<h2 class="header-bottom-form social-widgets-header">Присоединяйтесь к нам в социальных сетях!</h2>-->
 <!--            </div>-->
-            
-            <!--<div class="soc-widget-all">
-                <script type="text/javascript" src="//vk.com/js/api/openapi.js?136"></script>
--->
-                <!-- VK Widget -->
-               <!-- <div style="/*margin-left: auto; margin-right: auto;*/ float: right; margin-bottom: 50px;"
-                     class="soc-widget" id="vk_groups"></div>-->
-               <!-- <script type="text/javascript">
-                    VK.Widgets.Group("vk_groups", {
-                        mode: 0,
-                        width: "350",
-                        height: "400",
-                        color1: '0093D0',
-                        color2: 'FFFFFF',
-                        color3: 'FFFFFF'
-                    }, 134707555);
-                </script>-->
 
-                <!--<iframe
-                    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fborispol.coraltravel%2F&tabs=timeline&width=350&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
-                    width="350" height="400" style="border:none;/*overflow:hidden;*/z-index: 100;" scrolling="no" frameborder="0"
-                    allowTransparency="true"></iframe>-->
+<!--<div class="soc-widget-all">
+    <script type="text/javascript" src="//vk.com/js/api/openapi.js?136"></script>
+-->
+<!-- VK Widget -->
+<!-- <div style="/*margin-left: auto; margin-right: auto;*/ float: right; margin-bottom: 50px;"
+      class="soc-widget" id="vk_groups"></div>-->
+<!-- <script type="text/javascript">
+     VK.Widgets.Group("vk_groups", {
+         mode: 0,
+         width: "350",
+         height: "400",
+         color1: '0093D0',
+         color2: 'FFFFFF',
+         color3: 'FFFFFF'
+     }, 134707555);
+ </script>-->
+
+<!--<iframe
+    src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fborispol.coraltravel%2F&tabs=timeline&width=350&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+    width="350" height="400" style="border:none;/*overflow:hidden;*/z-index: 100;" scrolling="no" frameborder="0"
+    allowTransparency="true"></iframe>-->
 <!--            </div>-->
-       <!-- </div>
-    </div>
+<!-- </div>
+</div>
 </div>
 </div><!-- #content -->
 
